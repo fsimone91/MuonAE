6867ab92e205666080c72794978d8fde595cc31b
fsimone <federica.maria.simone@cern.ch>
2024-01-19 10:42:41 +0100

    minor updatesx

---
+++ README.md
@@ -0,0 +1,3 @@
+source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh 
+
+ python autoencoder_1d.py /eos/user/f/fsimone/auto_DQM/output_nanodqm/nanodqmio_2023C_Muon0_FEDTotalEventSize_mod.csv -p -v
--- autoencoder_1d.py
+++ autoencoder_1d.py
@@ -15,16 +15,25 @@ from tensorflow.keras.models import load_model
 from json_utils import *
 from data_preparation import *
 
-##TO DO arg parse
 
-verbose = True
+import argparse
+
+parser = argparse.ArgumentParser(description='Train and evaluate an autoencoder on 1D histograms.')
+parser.add_argument('inputpath', type=str,
+                    help='input csv complete abs path')
+parser.add_argument('-p', '--doplot',
+                    action='store_true')
+parser.add_argument('-v', '--verbose',
+                    action='store_true')
 
 # note: this script assumes you have a csv file stored at the specified location,
 #     containing only histograms of the specified type
 
 histname = 'FEDTotalEventSize'
+#filename = 'nanodqmio_2023C_Muon0_'+histname+'.csv'
 filename = 'nanodqmio_2023C_Muon0_'+histname+'_mod.csv'
 datadir = '/eos/user/f/fsimone/auto_DQM/output_nanodqm/'
+
 
 def mseTop10(y_true, y_pred):
     ### MSE top 10 loss function for autoencoder training
@@ -107,26 +116,81 @@ def train_simple_autoencoder(hists, nepochs=-1, modelname='',
   else: return (model, history)
 
 if __name__ == "__main__":
+
+  args = parser.parse_args()
+  doplot = args.doplot
+
+  csv_filename = args.inputpath.split('/')[-1]
   # load the file into a dataframe
-  df = pd.read_csv(datadir+filename)
-  if verbose:
+  df = pd.read_csv(args.inputpath)
+  if args.verbose:
     msg = 'INFO in autoencoder_1d:'
     msg += ' loaded a dataframe with {} rows and {} columns.'.format(len(df), len(df.columns))
     print(msg)
   
   ### filtering: select only DCS-bit on data
   ### TO-DO: filter out low statistics
-  df = select_dcson(df)
+  ### df = select_dcson(df) ##removed as I am using a modified input file
   print('number of passing lumisections after DCS selection: {}'.format( len(df) ))
   
   ### preprocessing of the data: rebinning and normalizing
-  X_train = get_hist_values(df)[0]  # returns np array of shape (nhists,nbins) (for 1D hists)
+  #X_train = get_hist_values(df)[0]  # returns np array of shape (nhists,nbins) (for 1D hists)
+  X_train,runs,ls = get_hist_values(df)
+  print(runs)
+  exit
   X_train = rebin1D(X_train,2)
   X_train = norm1D(X_train)
 
+  ### building and training model
   input_size = X_train.shape[1]
   arch = [int(X_train.shape[1]/2.)]
   act = ['tanh']*len(arch)
   opt = 'adam'
   loss = mseTop10
   autoencoder = getautoencoder(input_size,arch,act,opt,loss) 
+  history = autoencoder.fit(X_train, X_train, epochs=20, batch_size=500, shuffle=False, verbose=args.verbose, validation_split=0.1)
+
+  ### evaluate the model on the training set
+  prediction_train = autoencoder.predict(X_train)
+  mse_train = mseTop10(X_train, prediction_train)
+  mse_train = mse_train.numpy() ##convert EagerTensor into numpy array
+
+  print(mse_train.mean(), mse_train.std())
+
+  ## recompute mse mean and std removing outliers
+  ## threshold = np.quantile(mse_train,1-0.005)
+  ## mse_train = mse_train[mse_train<threshold]
+  gmean = mse_train.mean()
+  gstd = mse_train.std()
+
+  print(gmean, gstd)
+
+  ### define anomalous instances
+  n = 3 #sigmas
+  selection = abs(mse_train-gmean)>n*gstd
+  print(mse_train[selection])
+  print(runs[selection])
+  print(ls[selection])
+
+  if(doplot):
+    ## plot
+    fig,ax = plt.subplots()
+    fig.set_size_inches(8, 6)
+    
+    #x axis
+    ax.hlines(gmean,            ls.min(),ls.max(), color='b', label='average: {}'.format(gmean))
+    ax.hlines(gmean+(1.0*gstd), ls.min(),ls.max(), color='r', label='1 sigma ({})'.format(gstd))
+    ax.hlines(gmean-(1.0*gstd), ls.min(),ls.max(), color='r', label='1 sigma ({})'.format(gstd))
+    ax.hlines(gmean+(3.0*gstd), ls.min(),ls.max(), color='r', label='3 sigma', linestyle=':')
+    ax.hlines(gmean-(3.0*gstd), ls.min(),ls.max(), color='r', label='3 sigma', linestyle=':')
+    
+    ax.set_ylim(np.min(mse_train)*0.9,np.max(mse_train)*1.1)
+    ax.scatter(ls, mse_train, marker='+', label='data points')
+    ax.scatter(ls[selection], mse_train[selection], marker='*', label='anomalous instances', color='r')
+    #ax.set_ylim([gmean - 5*gstd, gmean + 5*gstd])
+    ax.set_title(csv_filename)
+    ax.set_xlabel("LS")
+    ax.set_ylabel("MSE")
+    ax.legend()
+    plt.show()
+
